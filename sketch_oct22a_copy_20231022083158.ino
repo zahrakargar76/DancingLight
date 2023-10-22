2d2d5a2ed5d9bb4ea07f8571f4392707a0196924
zahrakargar76 <148703264+zahrakargar76@users.noreply.github.com>
2023-10-22 08:35:18 -0700

    Add files via upload

---
+++ sketch_oct22a_copy_20231022083158.ino
@@ -0,0 +1,24 @@
+//definition of two dimensional presentation
+int danclight [5] [4] = {
+  {0,0,0,1},
+  {0,0,1,1},
+  {0,1,1,0},
+  {1,1,0,0},
+  {1,0,0,0}
+}
+void setup() {
+  // put your setup code here, to run once:
+  for(int thispin = 0; thispin < 4; thispin++) {
+    pinMode(thispin, OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int a = 0; a <= 5; a++){
+    for(int i = 0; i <= 4; i++){
+      digitalWrite(i, danclight[a] [i]);
+    }
+    delay(500); //wait for a second
+  }
+}
